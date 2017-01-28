33d9d4e843acac30f3a33a8c6ebc14ed3938b06f
Dominic Gunn <Dominic.Gunn@sulake.com>
2017-01-28 12:38:22 +0000

    Add support for EU and bump versions.

--- src/com/vain/flicker/utils/Shard.java
+++ src/com/vain/flicker/utils/Shard.java
@@ -4,5 +4,24 @@ package com.vain.flicker.utils;
  * @author Dominic Gunn (dominic@vain.gg)
  */
 public enum Shard {
-    NA
+    NA("North America"), EU("Europe");
+
+    private String shardRegion;
+
+    Shard(String shardRegion) {
+        this.shardRegion = shardRegion;
+    }
+
+    public String getShardRegion() {
+        return shardRegion;
+    }
+
+    public static Shard getShardByShortCode(String shortCode) {
+        for (Shard shard : Shard.values()) {
+            if (shard.name().equalsIgnoreCase(shortCode)) {
+                return shard;
+            }
+        }
+        return null;
+    }
 }
