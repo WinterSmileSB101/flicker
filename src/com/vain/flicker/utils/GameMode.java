07a167799064da8805ad2dae74a2b467b7cec949
Dominic Gunn <Dominic.Gunn@sulake.com>
2017-02-16 20:44:54 +0000

    Add game modes.

---
+++ src/com/vain/flicker/utils/GameMode.java
@@ -0,0 +1,19 @@
+package com.vain.flicker.utils;
+
+/**
+ * @author Dominic Gunn (dominic@vain.gg)
+ * TODO: Figure out good usecase for this, perhaps an enum?
+ */
+public class GameMode {
+    public static final String RANKED = "ranked";
+    public static final String CASUAL = "casual";
+
+    public static final String BLITZ = "blitz_pvp_ranked";
+    public static final String BATTLE_ROYAL = "casual_aral";
+
+    public static final String PRIVATE_CASUAL = "private";
+    public static final String PRIVATE_RANKED = "private_party_draft_match";
+
+    public static final String PRIVATE_BLITZ = "private_party_blitz_match";
+    public static final String PRIVATE_BATTLE_ROYAL = "private_party_aral_match";
+}
