ab0d25ec591a438286f6eff75bba25216aea342a
Dominic Gunn <Dominic.Gunn@sulake.com>
2017-02-27 21:34:02 +0000

    Update player to latest schema.

--- src/com/vain/flicker/model/player/PlayerStats.java
+++ src/com/vain/flicker/model/player/PlayerStats.java
@@ -8,10 +8,20 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PlayerStats {
 
+    private int wins;
     private int winStreak;
     private int lossStreak;
 
-    private int lifetimeGold;
+    private int xp;
+    private int level;
+    private double lifetimeGold;
+
+    private int played;
+    private int playedRanked;
+
+    public int getWins() {
+        return wins;
+    }
 
     public int getWinStreak() {
         return winStreak;
@@ -21,16 +31,37 @@ public class PlayerStats {
         return lossStreak;
     }
 
-    public int getLifetimeGold() {
+    public int getXp() {
+        return xp;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public double getLifetimeGold() {
         return lifetimeGold;
+    }
+
+    public int getPlayed() {
+        return played;
+    }
+
+    public int getPlayedRanked() {
+        return playedRanked;
     }
 
     @Override
     public String toString() {
         return "PlayerStats{" +
-                "winStreak=" + winStreak +
-                ", lossStreak=" + lossStreak +
+                "playedRanked=" + playedRanked +
+                ", played=" + played +
                 ", lifetimeGold=" + lifetimeGold +
+                ", level=" + level +
+                ", xp=" + xp +
+                ", lossStreak=" + lossStreak +
+                ", winStreak=" + winStreak +
+                ", wins=" + wins +
                 '}';
     }
 }
